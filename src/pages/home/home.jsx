cf762be53fdc117353347844ce3e721323b28c29
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2023-07-21 23:46:30 +0300

    create map component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import { Col, Row } from "antd";
 import { MainLayout } from "../../components/layout/layout";
 import { RoutesList } from "../../components/routes-list/routes-list";
+import { Map } from "../../components/map/map";
 
 export const HomePage = () => {
   return (
@@ -9,7 +10,9 @@ export const HomePage = () => {
         <Col span={10}>
           <RoutesList />
         </Col>
-        <Col span={14}></Col>
+        <Col span={14}>
+          <Map/>
+        </Col>
       </Row>
     </MainLayout>
   );
